1e12aa41f6d74c6df1b02a526839c796e3fb15df
Keante Young <youngkeante@gmail.com>
2024-11-13 22:15:03 -0500

    Updated the BreadSize Enum so that it shows private final "int".

--- KrustySubz/src/main/java/com/pluralsight/deli/BreadSize.java
+++ KrustySubz/src/main/java/com/pluralsight/deli/BreadSize.java
@@ -1,30 +1,14 @@
 package com.pluralsight.deli;
 
 public enum BreadSize {
-    Small(4), Medium(8), Large(12);
+    SMALL(4),
+    MEDIUM(8),
+    LARGE(12);
 
-    private final int inches;
+    private final int i;
 
-    BreadSize(int inches) {
-        this.inches = inches;
-    }
-
-    public int getInches() {
-        return inches;
-    }
-
-    public double getPriceAdjustment() {
-        switch (this) {
-
-            case Small:
-                return 6.20;
-            case Medium:
-                return 8.45;
-            case Large:
-                return 10.85;
-            default:
-                return 0.0;
-        }
+    BreadSize(int i) {
+        this.i = i;
     }
 
 }
