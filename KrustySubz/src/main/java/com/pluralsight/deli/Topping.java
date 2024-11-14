76bfd86e6b98efe1177f8a41bb5b098358bb49ce
Keante Young <youngkeante@gmail.com>
2024-11-13 23:16:15 -0500

    Added all "Toppings" into one Enum.

---
+++ KrustySubz/src/main/java/com/pluralsight/deli/Topping.java
@@ -0,0 +1,46 @@
+package com.pluralsight.deli;
+
+public enum Topping {
+
+    //Meat choices
+
+    STEAK("Meat"),
+    HAM("Meat"),
+    SALAMI("Meat"),
+    ROAST_BEEF("Meat"),
+    CHICKEN("Meat"),
+    BACON("Meat"),
+
+    //Cheese options
+
+    AMERICAN("Cheese"),
+    PROVOLONE("Cheese"),
+    CHEDDAR("Cheese"),
+    SWISS("Cheese"),
+
+    //Regular toppings
+
+    Lettuce("Veggie"),
+    Peppers("Veggie"),
+    Onions("Veggie"),
+    Tomatoes("Veggie"),
+    Jalepenos("Veggie"),
+    Cucumbers("Veggie"),
+    Pickles("Veggie"),
+    Guacamole("Veggie"),
+    Mushrooms("Veggie");
+
+
+
+
+    private final String toppingType;
+
+    Topping(String toppingType){
+        this.toppingType = toppingType;
+
+    }
+
+    public String getToppingType() {
+        return toppingType;
+    }
+}
