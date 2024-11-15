7a4719ef2b9337a8bb803c7cae07802da0ed9f19
Keante Young <youngkeante@gmail.com>
2024-11-15 13:14:06 -0500

    Created the Chips class. It has a constructor along with a getter.

--- KrustySubz/src/main/java/com/pluralsight/deli/Chips.java
+++ KrustySubz/src/main/java/com/pluralsight/deli/Chips.java
@@ -1,10 +1,40 @@
 package com.pluralsight.deli;
 
-public enum Chips {
-    CLASSIC,
-    SALT_AND_VINEGAR,
-    JALAPENO_KETTLE_COOKED,
-    PICKLE,
-    SEA_SALT,
-    BBQ;
+import com.pluralsight.deli.model.Item;
+
+public class Chips extends Item {
+
+    private String chipFlavor;  // The flavor of the chips
+    private static final double price = 1.50;  // The fixed price for chips
+
+    // created a constructor to initialize the chip flavor
+    public Chips(String chipFlavor) {
+        super("Chips", price);  // calling on the super class (Item) with the name and price
+        this.chipFlavor = chipFlavor;
+    }
+
+    public String getChipFlavor() {
+        return chipFlavor;
+    }
+
+    public void setChipFlavor(String chipFlavor) {
+        this.chipFlavor = chipFlavor;
+    }
+
+    @Override
+    public double getExtraPrice() {
+        return 0;
+    }
+
+    @Override
+    public double totalPrice() {
+        return 0;
+    }
+
+    @Override
+    public String toString() {
+        return "Chips{" + "chipFlavor='" + chipFlavor + '\'' + ", price=" + price + '}';
+    }
 }
+
+
